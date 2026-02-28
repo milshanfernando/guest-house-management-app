a7035e40bd1626a23f37016c88fd06d9ee553737
milshanfernando <milshanfernando.mf@gmail.com>
2026-02-28 13:15:21 +0400

    changes

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -7,9 +7,9 @@ import { ClipboardList, Bed, Home, Wallet, X } from "lucide-react";
 const menu = [
   { name: "Bookings", href: "/bookings", icon: ClipboardList },
   { name: "Bulk Bookings", href: "/bulk-bookings", icon: ClipboardList },
-  { name: "Rooms", href: "/rooms", icon: Bed },
+  // { name: "Rooms", href: "/rooms", icon: Bed },
   { name: "Room Occupancy", href: "/room-occupancy", icon: Home },
-  { name: "Daily Income", href: "/incomes", icon: Wallet },
+  // { name: "Daily Income", href: "/incomes", icon: Wallet },
 ];
 
 export default function Sidebar({
